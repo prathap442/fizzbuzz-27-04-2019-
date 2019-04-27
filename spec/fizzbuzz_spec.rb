fa6c460bf9d4b58119e03497df8e7ef16ed7b18c
prathap <prathap.mohan@kloctechnologies.co.in>
2019-04-27 14:50:15 +0530

    implemented the logic of divisibility by 5 and 3 and both

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,12 +3,19 @@ require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
   it "returns fizz if the number is divisible by 3" do
-    expect(fizzbuzz(3)).to eq('fizz')
+    expect(fizzbuzz(3)).to eq('fizz!')
   end
   it "return should accept 9 and return fizz" do 
-    expect(fizzbuzz(9)).to eq('fizz')
+    expect(fizzbuzz(9)).to eq('fizz!')
   end
   it "returns 2 as it is not divisble by 3" do 
     expect(fizzbuzz(2)).to eq(2)
+  end
+  it "return buzz when the 5 is inputted" do 
+    expect(fizzbuzz(5)).to eq('buzz!')
+  end
+
+  it "returns buzz when the 15 is inputted" do 
+    expect(fizzbuzz(15).to eq('fizzbuzz!'))
   end
 end
