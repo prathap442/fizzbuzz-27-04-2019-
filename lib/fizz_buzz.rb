2b7e516876b943ce2b82c48181ab916aeca7be41
prathap <prathap.mohan@kloctechnologies.co.in>
2019-04-27 15:18:16 +0530

    implementation of the sad path is done here

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,10 @@
+require 'pry'
 def fizzbuzz(number)
+  binding.pry  
+  until number.is_a?(Integer) do  #Fixnum is deprecated in ruby 2.40
+    print "enter the number\n"
+    number = Integer(gets) rescue nil 
+  end
   if(((number%3)==0) && ((number%5)== 0))
     return "fizzbuzz!"  
   elsif((number%3) == 0)
@@ -12,3 +18,7 @@ end
 
 #we can load the file in the irb and then
 #test the file by using load 'lib/fizz_buzz.rb'
+
+
+#now we see the sad paths taken into consideration
+#the sad paths are the one's like what we donot expect from the user
