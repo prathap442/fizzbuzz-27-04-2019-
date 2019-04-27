7a8814b8d0c6281b393a9c746e4511a03dae1fca
prathap <prathap.mohan@kloctechnologies.co.in>
2019-04-27 13:44:31 +0530

    made a test case

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,7 @@ describe 'fizz_buzz' do
   it "return should accept 9 and return fizz" do 
     expect(fizzbuzz(9)).to eq('fizz')
   end
+  it "returns 2 as it is not divisble by 3" do 
+    expect(fizzbuzz(2)).to eq(2)
+  end
 end
